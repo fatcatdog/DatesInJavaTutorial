ee5c8c31c1d56f33d1738505068be686a2570e8f
jacob <jacob@jacobs-Air.home>
2019-08-27 01:06:18 -0400

    first commit

---
+++ DatesInJava/src/unfinished/NotFinishedDailyPractice.java
@@ -0,0 +1,34 @@
+package unfinished;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class NotFinishedDailyPractice {
+	
+	private static final DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		
+		public static void main(String[] args) throws ParseException {
+			
+//			1. Lets print today's date without any type of manipulation
+			
+			
+//			2. Lets try print out our date object with the help of our date formatter
+
+			
+//			3. Lets try print tomorrows date. Maybe we should try out the util.calendar helper package?
+			
+
+//			4. Lets try print yesterday's date. Maybe we should try out the util.calendar helper package?
+			
+			
+//			5. For those who finished early, lets build a quick program to run in the console 
+//			that allows users to input their birth date. We can then tell them how many day's they have been alive!
+//			We can also tell them how many day's until their birthday
+			
+	      
+		}
+}
